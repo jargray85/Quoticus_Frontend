05aeff43e2f628690122a0ed6133088f429cc6f1
JamesRJGray <jamesrjgray@gmail.com>
2023-06-09 23:09:57 -0500

    categoryquotes alteration

--- quoticus/src/pages/CategoryQuotes.tsx
+++ quoticus/src/pages/CategoryQuotes.tsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getCategoryQuotes } from '../api/api';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getCategoryQuotes } from '../api/api'
 
 interface Author {
-    id: number;
-    name: string;
-    date: string;
-    quote: string;
-    source: string;
+    id: number
+    name: string
+    date: string
+    quote: string
+    source: string
 }
 
 interface CategoryQuotesResponse {
-    authors: Author[];
-    category_name: string;
-    status: number;
+    authors: Author[]
+    category_name: string
+    status: number
 }
 
 const CategoryQuotes = () => {
     const { categoryName } = useParams();
-    const [authors, setAuthors] = useState<Author[]>([]);
+    const [authors, setAuthors] = useState<Author[]>([])
 
     useEffect(() => {
         if (categoryName) {
-            fetchCategoryQuotes();
+            fetchCategoryQuotes()
         }
-    }, [categoryName]);
+    }, [categoryName])
 
     const fetchCategoryQuotes = async () => {
         try {
-            console.log('Fetching category quotes...');
+            console.log('Fetching category quotes...')
 
-            const response = await getCategoryQuotes(categoryName!);
-            console.log('API Response:', response);
+            const response = await getCategoryQuotes(categoryName!)
+            console.log('API Response:', response)
 
             if (response && response.status === 200 && response.data && response.data.authors) {
-                setAuthors(response.data.authors);
+                setAuthors(response.data.authors)
             } else {
-                setAuthors([]);
+                setAuthors([])
             }
         } catch (error) {
-            console.error('Error fetching category quotes:', error);
+            console.error('Error fetching category quotes:', error)
         }
     };
 
@@ -64,7 +64,7 @@ const CategoryQuotes = () => {
                 <p>No authors found for the category.</p>
             )}
         </div>
-    );
-};
+    )
+}
 
-export default CategoryQuotes;
+export default CategoryQuotes
