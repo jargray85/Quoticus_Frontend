9e391255f4bd0a2f9dadd0329f018860576d46d9
JamesRJGray <jamesrjgray@gmail.com>
2023-06-12 20:10:39 -0500

    initial styling of home page. will tweak later

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,23 @@ import React from "react"
 import { Link } from "react-router-dom"
 import greekPillars from "../assets/greek-pillars.png"
 
+
 // Home page
 const Home = () => {
     return (
         <div>
           <h1>"Quoticus"</h1>
-          <div>
+          <div className="link-container">
             <ul>
-              <li>
+              <li className="home-link">
                 <Link to="/authors">Authors</Link>
                 <br></br>
-                <img src={greekPillars} alt="Greek Pillars" />
+                <img src={greekPillars} alt="Greek Pillars" className="image-style" />
               </li>
-              <li>
+              <li className="home-link">
                 <Link to="/categories">Categories</Link>
                 <br></br>
-                <img src={greekPillars} alt="Greek Pillars" />
+                <img src={greekPillars} alt="Greek Pillars" className="image-style" />
               </li>
             </ul>
           </div>
