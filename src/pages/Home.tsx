18f5aaf42c9f81ef79ba37001a1566f2c732a6ea
JamesRJGray <jamesrjgray@gmail.com>
2023-06-16 13:48:27 -0500

    added some styling for mobile hopefully and attempted to toggle title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import greekPillars from "../assets/greek-pillars.png"
 import laurel from "../assets/laurel.png"
@@ -6,9 +6,19 @@ import laurel from "../assets/laurel.png"
 
 // Home page
 const Home = () => {
+  const [showMotto, setShowMotto] = useState(false);
+
+  const handleToggleMotto = () => {
+    setShowMotto(!showMotto)
+  }
+
+
     return (
         <div className="container">
-          <h1>"Quoticus"</h1>
+          <h1 onClick={handleToggleMotto}>"Quoticus"</h1>
+          {showMotto && (
+          <h5>Quotes from the dead, to better the living</h5>
+          )}
           <img src={laurel} alt="laurel leaf" className="home-image" />
           <div className="link-container">
             <ul>
